0e71c4333f926a9532d4dc9341a0053333c3e568
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2026-04-28 20:38:50 +0600

    build(webpack): add admin/index entry point

--- webpack.config.js
+++ webpack.config.js
@@ -11,22 +11,15 @@ const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 const updatedConfig = {
 	...defaultConfig,
 
-	// entry: {
-	// 	...defaultConfig.entry,
-	//
-	// 	// Individual block entries
-	// 	'blocks/author-profile/index': './src/blocks/author-profile/index.js',
-	// 	'blocks/author-grid/index': './src/blocks/author-grid/index.js',
-	// 	'blocks/author-carousel/index': './src/blocks/author-carousel/index.js',
-	// 	'blocks/author-carousel/view': './src/blocks/author-carousel/view.js',
-	// 	'blocks/author-list/index': './src/blocks/author-list/index.js',
-	// },
+	entry: {
+		...defaultConfig.entry,
+		'admin/index': './src/admin/index.tsx',
+	},
 
 	resolve: {
 		...defaultConfig.resolve,
 		alias: {
 			...defaultConfig.resolve?.alias,
-			// Path alias for src directory
 			'@': path.resolve( process.cwd(), 'src' ),
 		},
 	},
