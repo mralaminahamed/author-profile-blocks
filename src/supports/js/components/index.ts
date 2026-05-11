88ab7d241b66de6cdc7aa9e891cc134cba3569fb
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2026-05-12 01:28:03 +0600

    refactor(blocks): drop dead author selector components
    AuthorBlockPlaceholder replaced these earlier; nothing in edit.tsx files
    imports them anymore. Remove:
    - src/blocks/author-profile/components/AuthorSelector.tsx
    - src/supports/js/components/AuthorPicker.tsx
    - src/supports/js/components/AuthorSelector.tsx
    - src/supports/js/components/MultiAuthorSelector.tsx
    Trim the supports index export to the two components still in use.

--- src/supports/js/components/index.ts
+++ src/supports/js/components/index.ts
@@ -3,7 +3,4 @@
  */
 
 export { default as AuthorBlockPlaceholder } from './AuthorBlockPlaceholder';
-export { default as AuthorPicker } from './AuthorPicker';
 export { default as AuthorsListPreview } from './AuthorsListPreview';
-export { default as MultiAuthorSelector } from './MultiAuthorSelector';
-export { default as AuthorSelector } from './AuthorSelector';
